a565f8c7c7d32ba4f093f530eef9306f8e28f209
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-03-15 10:33:40 -0600

    fixed while loop condition

--- popGrowth.js
+++ popGrowth.js
@@ -1,7 +1,7 @@
 function popGrowth(popStart, percent, people, popSurpass){
     let years = 0
     let percentDec = percent / 100
-    while(popStart < popSurpass){
+    while(popStart < popSurpass && popStart > 0){
         let newPop = popStart + (popStart * percentDec) + people
         popStart = newPop
         years++
@@ -9,19 +9,5 @@ function popGrowth(popStart, percent, people, popSurpass){
     return years
 }
 
-console.log(popGrowth(1500000, 2.5, 10000, 2000000))
-
-
-function popGrowth1(popStart, percent, people){
-    let years = 0
-    let percentDec = percent / 100
-    while(popStart > 0){
-        let newPop = popStart + (popStart * percentDec) - people
-        popStart = newPop
-        years++
-    }
-    return years
-}
-
-console.log(popGrowth1(1500, 5, 100))
+console.log(popGrowth(1500000, 2.5, -10000, 2000000))
 
